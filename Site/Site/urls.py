58a0eb6c33ad2d192e8c5fffb0554d16959b072b
Yaacov Zerrad <yaazerrad@gmail.com>
2022-06-20 22:15:39 +0300

    fbv and cbv

--- Site/Site/urls.py
+++ Site/Site/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('blog/', include('blog.urls')),
+    path('courses/', include('courses.urls')),
     
 ]
 
